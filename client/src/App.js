7f84c058e61ba3be3086afc4bb1d1eaa3000e44d
Davelpz-tech <davelpz15@gmail.com>
2021-09-29 20:58:43 -0700

    adding main video content and carousel + styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,23 @@
 import './App.css';
 import Navbar from './components/Navbar.jsx'
+import Sidebar from './components/Sidebar';
+import Carousel from './components/Carousel';
+import { CarouselData } from './components/CarouselData';
+import Title from './components/Title';
+import Recommended from './components/Recommended';
 
 function App() {
   return (
     <div className='app'>
-      <Navbar/>
+      <Navbar />
+      <div class="app__main">
+        <Sidebar />
+        <div class="main__container">
+          <Title />
+          <Carousel slides={CarouselData} />
+          <Recommended />
+        </div>
+      </div>
     </div>
   );
 }
