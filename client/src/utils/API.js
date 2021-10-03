5fa8ee209dce5883e66c104bfc0cdb61b80ed2e9
Cole Sammons <colemsammons@gmail.com>
2021-10-02 17:56:07 -0700

    setup api to plug into the front end

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,10 +1,13 @@
 import env from "react-dotenv";
+
+//Get back search results for keyword
 export const getYtSearch = (data) => {
     data = data.replace(/\s+/g, '%20').toLowerCase().trim();
     const url = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=${data}&key=AIzaSyDQ-TXKonQ_KK--UuDHZIm6bexKVKgcE_o&maxResults=15`;
     return fetch(url);
 };
 
+//get back access token and expiration
 export const getTwToken = () => {
     const url = `https://id.twitch.tv/oauth2/token?client_id=${env.DB_TWITCH_CLIENT}&client_secret=${env.DB_TWITCH_SECRET}&grant_type=client_credentials`;
 
@@ -14,9 +17,10 @@ export const getTwToken = () => {
 
 };
 
+//get back <20 live channels for a game category, english only
 export const getTwChannelsByGameID = (token, gameID) => {
     const options = {
-        url: `https://api.twitch.tv/helix/streams?game_id=${gameID}&first=10&language=en`,
+        url: `https://api.twitch.tv/helix/streams?game_id=${gameID}&first=20&language=en`,
         headers: {
             'Client-Id': '0url3bsnihn8ffk5wq6ywyp55xu255',
             'Authorization': `Bearer ${token}`
@@ -29,9 +33,25 @@ export const getTwChannelsByGameID = (token, gameID) => {
     })
 };
 
-export const getTw= (token) => {
+//get back game categories that match value entered
+export const getTwCategoriesByGame= (token, game) => {
     const options = {
-        url: 'https://api.twitch.tv/helix/streams?game_id=33214&first=10&language=en',
+        url: `https://api.twitch.tv/helix/search/categories?query=${game}`,
+        headers: {
+            'Client-Id': '0url3bsnihn8ffk5wq6ywyp55xu255',
+            'Authorization': `Bearer ${token}`
+        }
+    };
+
+    return fetch(options.url, {
+        method: "GET",
+        headers: options.headers
+    })
+};
+
+export const getTwTopGames= (token) => {
+    const options = {
+        url: `https://api.twitch.tv/helix/games/top`,
         headers: {
             'Client-Id': '0url3bsnihn8ffk5wq6ywyp55xu255',
             'Authorization': `Bearer ${token}`
