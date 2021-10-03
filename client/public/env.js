838744ea8cad36dbafdabc649c25b773cb95d8b8
kama casta <kamacasta983@gmail.com>
2021-10-03 16:53:57 -0700

    added dependices, logic to chat function not yet completed

--- client/public/env.js
+++ client/public/env.js
@@ -1,5 +1,1 @@
-window.env = {
-  "DB_SECRET": "dogsaresmelly",
-  "DB_TWITCH_SECRET": "ljuvmycd0k3h67m0u8o7t393aj9xyc",
-  "DB_TWITCH_CLIENT": "0url3bsnihn8ffk5wq6ywyp55xu255"
-};
+window.env = {};
--- client/src/components/Chat.jsx
+++ client/src/components/Chat.jsx
@@ -1,9 +1,32 @@
 import React, { useEffect, useRef, useState } from "react"
 import "../style/Chat.css"
+import io from "socket.io-client"
+import TextField from "@material-ui/core/TextField"
 
+// connecting back end
+const socket = io.connect('http://localhost:3002')
 
 function Chat() {
+    // passing through objects
+    const [state, setState] = useState({message: ''})
+    // empty array for the objects message and username
+    const [chat, setChat] =  useState([])
+return(
+    <div className="chatSection">
+        <form onSubmit={}>
+            <h1>Chat</h1>
+            <div className="chatRendered">
+            {}
+            </div>
+            <TextField
 
+
+            />
+
+        </form>
+    
+    </div>
+)
 
 }
 export default Chat;
