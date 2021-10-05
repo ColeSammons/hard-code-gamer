6199c1766982a6b1a298a9b8dea3d375e527ebb6
Davelpz-tech <davelpz15@gmail.com>
2021-10-04 19:32:59 -0700

    fixing overflow css and containers

---
+++ client/src/components/Login.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import '../style/Login.css';
+
+const Login = () => {
+    return (
+        <form>
+            <div className="login-form">
+                <h2>LOGIN</h2>
+                
+                <div className="form-inputs">
+                    <label htmlFor="name">NAME: </label>
+                    <input type="text" name="name" id="name"/>
+                </div>
+                
+                <div className="form-inputs">
+                    <label htmlFor="email">EMAIL: </label>
+                    <input type="email" name="email" id="email" />
+                </div>
+
+                <div className="form-inputs">
+                    <label htmlFor="password">PASSWORD: </label>
+                    <input type="password" name="password" id="password"/>
+                </div>
+                <input type="submit" value="LOGIN" />
+                
+            </div>
+        </form>
+    )
+}
+
+export default Login
