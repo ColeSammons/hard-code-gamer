3caa7e5da7dc435e9c8ade73bceae64fa3db88a6
Cole Sammons <colemsammons@gmail.com>
2021-10-04 17:58:58 -0700

    api update

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -3,7 +3,12 @@ import env from "react-dotenv";
 //Get back search results for keyword
 export const getYtSearch = (data) => {
     data = data.replace(/\s+/g, '%20').toLowerCase().trim();
-    const url = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=${data}&key=${env.DB_YOUTUBE_ID}&maxResults=15`;
+    const url = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=${data}&key=${env.DB_YOUTUBE_ID}&maxResults=20&type=video`;
+    return fetch(url);
+};
+
+export const getYtRec = (vidID) => {
+    const url = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&relatedToVideoId=${vidID}&key=${env.DB_YOUTUBE_ID}&maxResults=20&type=video`;
     return fetch(url);
 };
 
@@ -17,10 +22,10 @@ export const getTwToken = () => {
 
 };
 
-//get back <20 live channels for a game category, english only
+//get back <50 live channels for a game category, english only
 export const getTwChannelsByGameID = (token, gameID) => {
     const options = {
-        url: `https://api.twitch.tv/helix/streams?game_id=${gameID}&first=20&language=en`,
+        url: `https://api.twitch.tv/helix/streams?game_id=${gameID}&first=50&language=en`,
         headers: {
             'Client-Id': '0url3bsnihn8ffk5wq6ywyp55xu255',
             'Authorization': `Bearer ${token}`
@@ -36,7 +41,7 @@ export const getTwChannelsByGameID = (token, gameID) => {
 //get back game categories that match value entered
 export const getTwCategoriesByGame= (token, game) => {
     const options = {
-        url: `https://api.twitch.tv/helix/search/categories?query=${game}`,
+        url: `https://api.twitch.tv/helix/search/categories?query=${game}&first=50`,
         headers: {
             'Client-Id': '0url3bsnihn8ffk5wq6ywyp55xu255',
             'Authorization': `Bearer ${token}`
@@ -49,9 +54,25 @@ export const getTwCategoriesByGame= (token, game) => {
     })
 };
 
+//get back top twitch games
 export const getTwTopGames= (token) => {
     const options = {
-        url: `https://api.twitch.tv/helix/games/top`,
+        url: `https://api.twitch.tv/helix/games/top?first=50`,
+        headers: {
+            'Client-Id': '0url3bsnihn8ffk5wq6ywyp55xu255',
+            'Authorization': `Bearer ${token}`
+        }
+    };
+
+    return fetch(options.url, {
+        method: "GET",
+        headers: options.headers
+    })
+};
+
+export const getTwTopChannels= (token) => {
+    const options = {
+        url: `https://api.twitch.tv/helix/streams?first=50&language=en`,
         headers: {
             'Client-Id': '0url3bsnihn8ffk5wq6ywyp55xu255',
             'Authorization': `Bearer ${token}`
