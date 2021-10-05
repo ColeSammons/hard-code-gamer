4cd374ef36667ae71098a01bb0dbd9f01c8cf7d9
Cole Sammons <colemsammons@gmail.com>
2021-10-05 14:53:14 -0700

    added links for signup and login, displays based on if user is logged in

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Auth from '../utils/auth';
 import Logo from '../assets/game-logo-test.png';
 import '../style/Navbar.css'; //import style for Nav component
 
@@ -6,25 +8,49 @@ const Navbar = () => {
     return (
         <div className='navbar'>
             <div className="nav__left">
-                <a href="/">
+                <Link to="/">
                     <img className='nav__leftLogo'
                         src={Logo}
                         alt="game logo" />
-                </a>
-                <h2>HARD CODE GAMERS</h2>
+                </Link>
+                <Link to="/" className="nav__leftName">
+                    <h2 >HARD CODE GAMERS</h2>
+                </Link>
             </div>
             <div className="nav__center">
-                <input type="text" placeholder="Search"/>
+                <input type="text" placeholder="Search" />
                 <div className="nav__centerLogoContainer">
                     <i className="fas fa-search"></i>
                 </div>
             </div>
             <div className="nav__right">
-                <div className="nav__rightContainer">
-                    <div className="nav__rightLogin">
-                        <h2>Login</h2>
+                {/* if user isn't logged in display login and signup, if they are then display logout */}
+                {Auth.loggedIn() ? (
+                    <>
+                        <div className="nav__rightContainer">
+                            <Link to="/login" className="nav__leftName">
+                                <div className="nav__rightLogin">
+                                    <h2>Login</h2>
+                                </div>
+                            </Link>
+                        </div>
+                        <div className="nav__rightContainer">
+                            <Link to="/signup" className="nav__leftName">
+                                <div className="nav__rightLogin">
+                                    <h2>Signup</h2>
+                                </div>
+                            </Link>
+                        </div>
+                    </>
+                ) : (
+                    <div className="nav__rightContainer">
+                        <a href="/" className="nav__leftName" onClick={() => Auth.logout()}>
+                            <div className="nav__rightLogin">
+                                <h2>Logout</h2>
+                            </div>
+                        </a>
                     </div>
-                </div>
+                )}
             </div>
         </div>
     )
