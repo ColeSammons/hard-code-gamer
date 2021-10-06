233307a778eb4dfda4d5913c9ed0c3b708de3350
Davelpz-tech <davelpz15@gmail.com>
2021-10-05 21:24:45 -0700

    adding avatars to sidebar

--- client/src/components/Channel.jsx
+++ client/src/components/Channel.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import '../style/Channel.css'
 
-const Channel = () => {
+const Channel = ({avatar, name, followers}) => {
     return (
         <div className="channel">
-            
+            <div className="channel__details">
+                <img src={avatar} alt="avatar" />
+                <p>{name}</p>
+            </div>
+            <div>Followers: {followers}</div>
         </div>
     )
 }
